ed9f42e177a2737d2d72b4d437b16702ebc05683
Kartik Gautam <kartikgautam171@gmail.com>
2022-04-19 23:50:57 +0530

    portfolio enhancement

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Portfolio
+          React Portfolio !
         </p>
         <a
           className="App-link"
