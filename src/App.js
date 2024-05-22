ff55edc1ea89e94b66dbaa9bf3e70117c0a8c242
Kartik Gautam <kartikgautam171@gmail.com>
2024-05-22 23:03:45 +0530

    init simple portfolio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import Navbar from "./components/navbar";
 import React from "react";
-import Home from "./components/Home";
+import Home from "./components/home";
 function App() {
-    const [Theme, setTheme] =React.useState('light')
-    if (localStorage.theme === 'dark' ){
-        document.documentElement.classList.add('dark')
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
+
 
   return (
-    <div className="App h-screen dark:bg-black dark:text-white">
-        {/* <Navbar currTheme = {Theme} changeTheme={setTheme}/> */}
-        <Home  currTheme = {Theme}/>
+    <div >
+      <Home/>
     </div> 
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+
+
+function Home(props) {
+
+  return (
+    <div>
+      <h1>Hello, Kartik!</h1>
+    </div>
+  );
+}
+
+export default Home;
