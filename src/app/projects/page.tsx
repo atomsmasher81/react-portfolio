9ed719f81675eaaf214d8677afdf09de5189a72d
Kartik Gautam <kartikgautam171@gmail.com>
2024-05-24 08:03:41 +0530

    Fixed project layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,32 +2,36 @@ import React from 'react';
 import {Card, CardContent} from "@/components/ui/card";
 import {PageWrapper} from "@/components/page-wrappper";
 
-const Blog = () => {
+const projects = [
+    {
+        title: 'Hereme',
+        description: 'Ready-to-use UI elements designed for rapid development.'
+    },
+    {
+        title: 'Prettyfolio',
+        description: 'A curated collection of portfolios for inspiration.'
+    },
+    {
+        title: 'Enchant',
+        description: 'A vibrant theme for Visual Studio Code.'
+    }
+
+]
+
+const Project = () => {
   return (
       <PageWrapper>
-          <div>
+          <div className="max-w-4xl mx-auto p-4 pt-0 sm:p-8">
               <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                  <Card className="w-full">
-                      <CardContent>
-                          {/*<CodeIcon className="h-6 w-6 mb-2" />*/}
-                          <h3 className="font-bold">syntaxUI</h3>
-                          <p>Ready-to-use UI elements designed for rapid development.</p>
-                      </CardContent>
-                  </Card>
-                  <Card className="w-full">
-                      <CardContent>
-                          {/*<ImageIcon className="h-6 w-6 mb-2" />*/}
-                          <h3 className="font-bold">Prettyfolio</h3>
-                          <p>A curated collection of portfolios for inspiration.</p>
-                      </CardContent>
-                  </Card>
-                  <Card className="w-full">
-                      <CardContent>
-                          {/*<EggIcon className="h-6 w-6 mb-2" />*/}
-                          <h3 className="font-bold">Enchant</h3>
-                          <p>A vibrant theme for Visual Studio Code.</p>
-                      </CardContent>
-                  </Card>
+                  {projects.map((project) => (
+                        <Card className="w-full" key={project.title}>
+                            <CardContent>
+                                <h3 className="font-bold pt-4">{project.title}</h3>
+                                <p className="pt-4">{project.description}</p>
+                            </CardContent>
+                        </Card>
+                  ))}
+
               </div>
           </div>
       </PageWrapper>
@@ -35,4 +39,4 @@ const Blog = () => {
   );
 };
 
-export default Blog;
+export default Project;
