518a38eadf8a54f56a2c2a76ab5990f89b122e91
Kartik Gautam <kartikgautam171@gmail.com>
2024-05-26 23:17:06 +0530

    fixed avatar on safari

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ function TopLayout() {
     return (
         <div className="max-w-4xl mx-auto p-4 pb-0 sm:p-8 sm:pb-0">
       <div className="flex flex-col items-center text-center">
-        <Avatar className="h-20 w-auto">
+        <Avatar className="h-20 w-auto aspect-square">
           <AvatarImage alt="Profile Picture" src="/img/img.png" />
         </Avatar>
         <h1 className="mt-4 mb-2 text-4xl sm:text-4xl ">Kartik Gautam</h1>
